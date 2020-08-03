5f8f72b8ab8934cc7d12d8490f0b272fad6678a9
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-03 21:39:29 +0100

    chore: passed props event

--- src/Components/SideDrawer/BackDrop.js
+++ src/Components/SideDrawer/BackDrop.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../SideDrawer/BackDrop.css";
 
-const BackDrop = (props) => <div className="backDrop"></div>;
+const BackDrop = (props) => (
+  <div className="backDrop" onClick={props.click}></div>
+);
 
 export default BackDrop;
