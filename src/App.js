4f50d739e476b75ecc3f7433a727e1cbe1d7ed12
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-03 20:22:17 +0100

    chore: imported backdrop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
+import BackDrop from "../src/Components/SideDrawer/BackDrop";
+import SideDrawer from "../src/Components/SideDrawer/SideDrawer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <SideDrawer />
+      <BackDrop />
       <p>Page content</p>
     </div>
   );
