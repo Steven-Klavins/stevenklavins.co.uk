b84ec17edbccfc7a2642edd0700df17bc44d0f78
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-03 17:50:14 +0100

    feat: added toggle button

---
+++ src/Components/SideDraw/ToggleButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../SideDraw/ToggleButton.css";
+
+const ToggleButton = (props) => (
+  <button className="toggleButton">
+    <span className="toggleLine"></span>
+    <span className="toggleLine"></span>
+    <span className="toggleLine"></span>
+  </button>
+);
+
+export default ToggleButton;
