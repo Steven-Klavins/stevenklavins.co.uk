1c5cdb1f8dd84ff5775067e9caa75a04879f4f6d
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-03 21:38:49 +0100

    feat: added onclick events for side draw

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import BackDrop from "../src/Components/SideDrawer/BackDrop";
 import SideDrawer from "../src/Components/SideDrawer/SideDrawer";
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <SideDrawer />
-      <BackDrop />
-      <p>Page content</p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    sideDrawerOpen: false,
+  };
+
+  drawClickHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+
+  backDropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+
+  render() {
+    let sideDrawer;
+    let backDrop;
+
+    if (this.state.sideDrawerOpen) {
+      sideDrawer = <SideDrawer />;
+      backDrop = <BackDrop click={this.backDropClickHandler} />;
+    }
+    return (
+      <div className="App">
+        <NavBar drawClickHandler={this.drawClickHandler} />
+        {sideDrawer}
+        {backDrop}
+
+        <p>Page content</p>
+      </div>
+    );
+  }
 }
 
 export default App;
