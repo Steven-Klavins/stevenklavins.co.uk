01ad1d9c6b11fcc710c72630fd701fd07b816d25
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-04 06:05:49 +0100

    chore: updated css classes

--- src/Components/SlideDrawer/BackDrop.js
+++ src/Components/SlideDrawer/BackDrop.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../SlideDrawer/BackDrop.css";
 
 const BackDrop = (props) => (
-  <div className="backDrop" onClick={props.click}></div>
+  <div className="backDrop backDropHide" onClick={props.click}></div>
 );
 
 export default BackDrop;
