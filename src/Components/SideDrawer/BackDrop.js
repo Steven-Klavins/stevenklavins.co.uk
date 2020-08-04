b63ac92ead3f6e5d19264012f551d2d80148d876
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-04 01:18:26 +0100

    chore: passed backdrop prop

--- src/Components/SideDrawer/BackDrop.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../SideDrawer/BackDrop.css";
-
-const BackDrop = (props) => (
-  <div className="backDrop" onClick={props.click}></div>
-);
-
-export default BackDrop;
