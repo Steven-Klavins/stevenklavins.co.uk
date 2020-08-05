0dc1341aaa8c5bec2e656679ef7daf7d7adbf62d
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-05 20:13:00 +0100

    chore: made background component

---
+++ src/Components/Background/Background.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../Background/Background.css";
+import logo from "../../images/main-logo.png";
+
+const Background = () => {
+  return <img className="bgimage" src={logo}></img>;
+};
+export default Background;
