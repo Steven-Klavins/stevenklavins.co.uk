7af92b776c99a975da2858b9f080206c34bb3391
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-06 13:47:38 +0100

    feat: added dev page

---
+++ src/Components/Pages/Coding/Coding.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../Pages/Coding/Coding.css";
+
+const Coding = (props) => {
+  return (
+    <div className="codeThing">
+      <h1>Coding</h1>
+    </div>
+  );
+};
+
+export default Coding;
