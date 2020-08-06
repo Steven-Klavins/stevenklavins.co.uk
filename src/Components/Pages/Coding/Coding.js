f0940a1f310c5e1e3e8dd575ad72a68fd8d82fce
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-06 19:26:08 +0100

    Chore: imported API data

--- src/Components/Pages/Coding/Coding.js
+++ src/Components/Pages/Coding/Coding.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "../../Pages/Coding/Coding.css";
+import GitHubAPI from "../../GitHubAPI/GitHubAPI";
 
 const Coding = (props) => {
   return (
-    <div className="codeThing">
-      <h1>Coding</h1>
+    <div>
+      <div className="upperMargin">
+        <h1 className="codeTitle">Notable Projects</h1>
+      </div>
+      <div className="space">
+        <GitHubAPI />
+      </div>
     </div>
   );
 };
