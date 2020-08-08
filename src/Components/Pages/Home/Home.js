db3432b38c18bd857fdbcf9769e60c77e3aa94e8
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-08 17:56:17 +0100

    feat: added video intro, file needs updating

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import ReactPlayer from "react-player";
 import "../../Pages/Home/Home.css";
 import IntroAnimation from "../../IntroAnimation/IntroAnimation";
 
@@ -9,7 +9,7 @@ const Home = (props) => {
       <h1 class="openTitle">Bridging the gap between tech and art</h1>
 
       <div className="welcomeTextBox">
-        <h2>Welcome!</h2>
+        <h2 className="subTitle">Welcome</h2>
         <p>
           Thank you so much for visiting my website! I'm a full-stack developer,{" "}
           <a
@@ -24,23 +24,23 @@ const Home = (props) => {
           remote worker and have the flexibility to relocate/travel if need be.{" "}
           <br></br>
           <br></br>I hold a BA in Digital Arts, perform in a band, and have a
-          love of all things creative, you can find my showreel{" "}
-          <a
-            className="midtextlink"
-            href="https://www.youtube.com/watch?v=oNoBRIztcew"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            here
-          </a>
-          . If your looking for someone with a yearning to make the world a
-          better place through innovative tech I'm your guy! <br></br>
+          love of all things creative. If your looking for someone with a
+          yearning to make the world a better place through innovative tech I'm
+          your guy! <br></br>
           <br></br>To be frank, its what gets me out of bed in the morning. Want
           to chat? Feel free to contact me on any of the listed platforms!
         </p>
       </div>
       <div>
         <IntroAnimation />
+      </div>
+      <div className="videoOuter">
+        <div className="video">
+          {/* <ReactPlayer
+            controls
+            url="https://www.dropbox.com/s/vj4anc4ijmy5ts9/Steven%20Klavins%202016-2018.mp4?raw=1"
+          /> */}
+        </div>
       </div>
     </div>
   );
