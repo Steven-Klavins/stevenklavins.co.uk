10eaec8a131eac9be2f9f5c941073cf9f237b75c
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-09 16:19:54 +0100

    feat: added 3D page

---
+++ src/Components/Pages/3D/3D.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../Pages/3D/3D.css";
+
+const threeD = () => {
+  return (
+    <div className="threeDMargin">
+      <h1 className="threeDTitle">3D Modelling/Printing</h1>
+    </div>
+  );
+};
+
+export default threeD;
