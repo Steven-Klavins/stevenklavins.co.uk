8f4a7d5b2a03bdaf6bc225b5db576518c695328d
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-09 16:17:09 +0100

    feat: added photography page

---
+++ src/Components/Pages/Photography/Photography.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../Pages/Photography/Photography.css";
+
+const Photography = () => {
+  return (
+    <div className="photographyMargin">
+      <h1 className="photographyTitle">Photography</h1>
+    </div>
+  );
+};
+
+export default Photography;
