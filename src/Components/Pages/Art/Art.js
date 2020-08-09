bbd222f7d90326a3e0ee018d58f6abf745460ca2
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-09 16:18:54 +0100

    feat: added art page

---
+++ src/Components/Pages/Art/Art.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../Pages/Art/Art.css";
+
+const Art = () => {
+  return (
+    <div className="artMargin">
+      <h1 className="artTitle">Art</h1>
+    </div>
+  );
+};
+
+export default Art;
