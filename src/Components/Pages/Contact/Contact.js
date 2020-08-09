c337fe535d4fc9d0ebb53d9a25771b40eabd9961
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-09 16:18:29 +0100

    feat: added contact page

---
+++ src/Components/Pages/Contact/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../../Pages/Contact/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contactMargin">
+      <h1 className="contactTitle">Contact</h1>
+      <p className="contactBox">
+        <span className="email">stevenklavins94@gmail.com</span> The best way to
+        contact me is via email, I will reply as promptly as possible.
+        Alternatively, you can contact me on any of the social media platforms
+        listed to the left, click on the links provided and you will be directed
+        to the relevant profiles.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
