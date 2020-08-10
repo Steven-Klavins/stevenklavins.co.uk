1c62b9eff250249b6c0071a455f34c6ac3fdaa08
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-11 00:08:43 +0100

    chore: added motion div

--- src/Components/Pages/Art/Art.js
+++ src/Components/Pages/Art/Art.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "../../Pages/Art/Art.css";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Art = () => {
   return (
-    <div className="artMargin">
+    <motion.div
+      exit={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      className="artMargin"
+    >
       <h1 className="artTitle">Art</h1>
-    </div>
+    </motion.div>
   );
 };
 
