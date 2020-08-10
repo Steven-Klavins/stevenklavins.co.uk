44b01a642aebe70f7d12ccf6ebdd3e605cb6b053
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-10 21:37:03 +0100

    feat: imported flickr gallery via API/FL

--- src/Components/Pages/Photography/Photography.js
+++ src/Components/Pages/Photography/Photography.js
@@ -1,30 +1,22 @@
 import React from "react";
 import "../../Pages/Photography/Photography.css";
+import FlickrLightbox from "react-flickr-lightbox";
 
 const Photography = () => {
   return (
     <div className="photographyMargin">
       <h1 className="photographyTitle">Photography</h1>
-
-      <div className="album">
-        <a
-          data-flickr-embed="true"
-          data-footer="true"
-          href="https://www.flickr.com/photos/189653295@N04/albums/72157715421892253"
-          title="Band Photography "
-        >
-          <img
-            src="https://live.staticflickr.com/65535/50206921332_bd39dffdc0_z.jpg"
-            width="427"
-            height="640"
-            alt="Band Photography "
-          ></img>
-        </a>
-        <script
-          async
-          src="//embedr.flickr.com/assets/client-code.js"
-          charset="utf-8"
-        ></script>
+      <h2 className="photographySubTitle">Band Photography</h2>
+      <div className="midAlbum">
+        <div>
+          <div className="album">
+            <FlickrLightbox
+              api_key={process.env.REACT_APP_API_KEY}
+              user_id="189653295@N04"
+              limit={12}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
