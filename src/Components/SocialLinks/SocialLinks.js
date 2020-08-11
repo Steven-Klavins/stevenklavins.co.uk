4f9040182018f498ee0d6f887769705f1ac0f51e
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-11 17:19:59 +0100

    chore: set links to open new tab

--- src/Components/SocialLinks/SocialLinks.js
+++ src/Components/SocialLinks/SocialLinks.js
@@ -9,28 +9,40 @@ const SocialLinks = (props) => (
   <div className="linkPosition">
     <div className="linkRow">
       <div className="section">
-        <a href="https://www.linkedin.com/in/steven-klavins-90b02a199/">
+        <a
+          href="https://www.linkedin.com/in/steven-klavins-90b02a199/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img className="imgLink" src={LinkedIn}></img>
           <span className="fontLink">LinkedIn</span>
         </a>
       </div>
 
       <div className="section">
-        <a href="https://github.com/Steven-Klavins">
+        <a
+          href="https://github.com/Steven-Klavins"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img className="imgLink" src={Github}></img>
           <span className="fontLink">GitHub</span>
         </a>
       </div>
 
       <div className="section">
-        <a href="https://twitter.com">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
           <img className="imgLink" src={Twitter}></img>
           <span className="fontLink">Twitter</span>
         </a>
       </div>
 
       <div className="section">
-        <a href="https://medium.com/@stevenklavins94">
+        <a
+          href="https://medium.com/@stevenklavins94"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img className="imgLink" src={Medium}></img>
           <span className="fontLink">Medium</span>
         </a>
