8934fe2eef0099244388072d38c88ecfe6f9ae90
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-14 18:00:46 +0100

    chore: updated description

--- src/Components/Pages/Music/Music.js
+++ src/Components/Pages/Music/Music.js
@@ -32,9 +32,27 @@ const Music = (props) => {
         </iframe>
 
         <p className="bandmsg">
-          Female fronted hard rock band from Croydon. Charliy Vaughan (Lead
-          Vocals) Steven Klavins (Lead Guitar/Backing Vocals) Stephen Mussett
-          (Rhythm Guitar) Ryan Nicholls (Bass Guitar) Phill Jones (Drum Kit)
+          Out Of Context is a female-fronted hard rock/metal band from Croydon
+          forming in 2015. The band consists of myself (lead guitar/backing
+          vocals), Charliy Vaughan (Lead Vocals) Stephen Mussett (Rhythm Guitar)
+          Ryan Nicholls (Bass Guitar), and Phill Jones (Drums).
+          <br></br>
+          <br></br>
+          In our time as a band, we have played a number of venues, written more
+          than an album's worth of songs (full-release to come soon), and
+          received an endorsement from{" "}
+          <a
+            href="http://www.stonewolfguitars.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            StoneWolf Guitars
+          </a>
+          . <br></br>
+          <br></br>
+          FaceBook <br></br>
+          Instagram <br></br>
+          YouTube <br></br>
         </p>
 
         <div>
