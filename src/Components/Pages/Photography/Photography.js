a33d74f2b00a703aad0b9a8b54950910f2c034fe
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-14 17:58:16 +0100

    chore: imported new image gallery

--- src/Components/Pages/Photography/Photography.js
+++ src/Components/Pages/Photography/Photography.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../../Pages/Photography/Photography.css";
 import FlickrLightbox from "react-flickr-lightbox";
 import { AnimatePresence, motion } from "framer-motion";
+import ImageGalleryAPI from "../ImageGallery/ImageGallery";
 
 const Photography = () => {
   return (
@@ -13,16 +14,21 @@ const Photography = () => {
     >
       <h1 className="photographyTitle">Photography</h1>
       <h2 className="photographySubTitle">Band Photography</h2>
-      <div className="midAlbum">
+      {/* <div className="midAlbum">
         <div>
           <div className="album">
             <FlickrLightbox
               api_key={process.env.REACT_APP_API_KEY}
               user_id="189653295@N04"
+              album_id="72157715469599206"
               limit={24}
             />
           </div>
         </div>
+      </div> */}
+      <h2 className="photographySubTitle">Nature Photography</h2>
+      <div className="gallery">
+        <ImageGalleryAPI gallery="72157715469599206" />
       </div>
     </motion.div>
   );
