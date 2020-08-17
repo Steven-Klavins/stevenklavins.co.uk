39d99627ac6273fd25f84fc95188bcfd7415f208
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-17 06:56:23 +0100

    feat: added gallery switch buttons

--- src/Components/Pages/Photography/Photography.js
+++ src/Components/Pages/Photography/Photography.js
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { Component, setState } from "react";
 import "../../Pages/Photography/Photography.css";
-import FlickrLightbox from "react-flickr-lightbox";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import ImageGalleryAPI from "../ImageGallery/ImageGallery";
 
-const Photography = () => {
-  return (
-    <motion.div
-      exit={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      className="photographyMargin"
-    >
-      <h1 className="photographyTitle">Photography</h1>
-      <h2 className="photographySubTitle">Band Photography</h2>
-      <div className="gallery">
-        <ImageGalleryAPI gallery="72157715488906066" limit={12} />
-      </div>
+const albums = ["72157715490137388", "72157715488906066"];
 
-      <h2 className="photographySubTitle">Nature Photography</h2>
-      <div className="gallery">
-        <ImageGalleryAPI gallery="72157715490137388" limit={12} />
-      </div>
-    </motion.div>
-  );
-};
+export default class Photography extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      album: 0,
+    };
+  }
 
-export default Photography;
+  render() {
+    return (
+      <motion.div
+        exit={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        className="photographyMargin"
+      >
+        <h1 className="photographyTitle">Photography</h1>
+        <h2 className="photographySubTitle">Band Photography</h2>
+        <li className="galleryTitles">
+          <button onClick={() => this.setState({ album: 0 })}>Nature</button>
+          <button onClick={() => this.setState({ album: 1 })}>
+            Band Photography
+          </button>
+          <button>Scenery</button>
+          <button>Misc</button>
+        </li>
+        <div className="gallery">
+          <ImageGalleryAPI gallery={albums[this.state.album]} limit={12} />
+        </div>
+      </motion.div>
+    );
+  }
+}
