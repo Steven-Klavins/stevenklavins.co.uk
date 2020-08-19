5e07111079fbf2ce0235c9e1cb39963b97b8855e
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-19 09:41:03 +0100

    chore: added alt image links

--- src/Components/Background/Background.js
+++ src/Components/Background/Background.js
@@ -3,6 +3,6 @@ import "../Background/Background.css";
 import logo from "../../images/main-logo.png";
 
 const Background = () => {
-  return <img className="bgimage" src={logo}></img>;
+  return <img className="bgimage" src={logo} alt="Steven Klavins Logo"></img>;
 };
 export default Background;
