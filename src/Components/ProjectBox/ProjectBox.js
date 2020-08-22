0560ae6df40bad5eaa13c9a495d1bf578cdeb514
Steven-Klavins <stevenklavins94@gmail.com>
2020-08-22 16:59:57 +0100

    chore: moved player position

--- src/Components/ProjectBox/ProjectBox.js
+++ src/Components/ProjectBox/ProjectBox.js
@@ -7,23 +7,24 @@ export default class ProjectBox extends Component {
     return (
       <div>
         <div className="projectBoxContainer">
-          <ReactPlayer
-            className="projectVideo"
-            url={this.props.params[0]}
-            style={{ "max-width": "86%" }}
-          />
-          <p className="projectDescription">
-            <h2 className="projectTitle">{this.props.params[2]}</h2>
-            {this.props.params[1]}
-          </p>{" "}
-        </div>
-        <div className="LRcontrols">
-          <button onClick={this.props.moveBack} className="arrowL ">
-            ◄
-          </button>
-          <button onClick={this.props.moveFoward} className="arrowR ">
-            ►
-          </button>
+          <h2 className="projectTitle">{this.props.params[2]}</h2>
+          <div className="LRcontrols">
+            <button onClick={this.props.moveBack} className="arrowL ">
+              ◄
+            </button>
+
+            <button onClick={this.props.moveFoward} className="arrowR ">
+              ►
+            </button>
+          </div>
+          <div className="projectBox">
+            <ReactPlayer
+              className="projectVideo"
+              url={this.props.params[0]}
+              style={{ "max-width": "86%" }}
+            />
+          </div>
+          <p className="projectDescription">{this.props.params[1]}</p>{" "}
         </div>
       </div>
     );
